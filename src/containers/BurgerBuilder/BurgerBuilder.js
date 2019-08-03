a8d5fe3c54008b24f540d2e574cc813a810d9429
Stephen Trewick <stephent818@gmail.com>
2019-08-02 19:57:56 -0700

    passes ingredients state

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,10 +4,20 @@ import Auxillary from '../../hoc/Auxillary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends React.Component {
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2
+        }
+    }
+
     render() {
         return(
             <Auxillary>
-                <Burger />
+                <Burger ingredients = {this.state.ingredients} />
                 <div>Build Controls</div>
             </Auxillary>
         );
